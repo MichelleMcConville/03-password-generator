6bf24ed5dcdb8055527baad3b85a57b7aad2208c
Michelle McConville <mchelmconvl@gmail.com>
2020-09-25 14:50:22 -0500

    added isNaN to very 1st prompt

--- script/script.js
+++ script/script.js
@@ -19,48 +19,40 @@ function writePassword() {
 
 // Function to generate random PW
 function generatePassword() {
-  var pwLength = prompt("Please make password between 8 to 128 characters long.");
-    console.log(pwLength);
+  var pwLength = prompt("Please make password between 8 to 128 characters long."); console.log(pwLength);
 
-      // Confirms to ask the user what to include in the PW
-      var upCaseAns = confirm("Click OK to use \"UPPER\" case letters OR Click Cancel");
-        console.log(upCaseAns);
-      var lowCaseAns = confirm("Click OK to use \"lower\" case letters OR Click Cancel");
-        console.log(lowCaseAns);
-      var numAns = confirm("Click OK to use \"Numbers\ OR Click Cancel");
-        console.log(numAns);
-      var specialCharAns = confirm("Click OK to use \"Special\" Characters OR Click Cancel");
-        console.log(specialCharAns);
+  // Checking the PW to make sure it is a # and correct length
+  if (isNaN(pwLength) || pwLength < 8 || pwLength >128) {
+    alert("Must enter a number between 8 & 128!");
+  } else {
 
-      // Alert to notify use that ONE selection must be made
-      if (!upCaseAns && !lowCaseAns && !numAns && !specialCharAns) {
-        alert("You must choose at least one option!");
-        console.log("You must choose at least one option!");
-      } else {
+    // Confirms to ask the user what to include in the PW
+    var upCaseAns = confirm("Click OK to use \"UPPER\" case letters OR Click Cancel"); console.log(upCaseAns);
+    var lowCaseAns = confirm("Click OK to use \"lower\" case letters OR Click Cancel"); console.log(lowCaseAns);
+    var numAns = confirm("Click OK to use \"Numbers\ OR Click Cancel"); console.log(numAns);
+    var specialCharAns = confirm("Click OK to use \"Special\" Characters OR Click Cancel"); console.log(specialCharAns);
 
-        if (upCaseAns) {
-          maxArray = maxArray.concat(upCase); console.log(maxArray);
-        }
+    // Alert to notify use that ONE selection must be made
+    if (!upCaseAns && !lowCaseAns && !numAns && !specialCharAns) {
+      alert("You must choose at least one option!");
+      console.log("You must choose at least one option!");
+    } else {
 
-        if (lowCaseAns) {
-          maxArray = maxArray.concat(lowCase); console.log(maxArray);
-        }
+      // Concatenation of main arrays
+      if (upCaseAns) {maxArray = maxArray.concat(upCase);} console.log(maxArray);
+      if (lowCaseAns) {maxArray = maxArray.concat(lowCase);} console.log(maxArray);
+      if (numAns) {maxArray = maxArray.concat(num);} console.log(maxArray);
+      if (specialCharAns) {maxArray = maxArray.concat(specialChar);} console.log(maxArray);
 
-        if (numAns) {
-          maxArray = maxArray.concat(num); console.log(maxArray);
-        }
+      // For Loop for random number generation
+      for (var i = 0; i < pwLength; i++) {
+        newUserPw = newUserPw + maxArray[Math.floor(Math.random() * maxArray.length)];
+      }
 
-        if (specialCharAns) {
-          maxArray = maxArray.concat(specialChar); console.log(maxArray);
-        }
-
-        for (var i = 0; i < pwLength; i++) {
-          newUserPw = newUserPw + maxArray[Math.floor(Math.random() * maxArray.length)];
-        }
-
-      // Returning the random generated PW to the user
-      return newUserPw;  
-      }  
+    // Returning the random generated PW to the user
+    return newUserPw;  
+    }
+  } 
 }
 
 // Add event listener to generate button
