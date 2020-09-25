d03e4889dce09431255f7626829f5d88183cd4ac
Michelle McConville <mchelmconvl@gmail.com>
2020-09-25 14:25:27 -0500

    added non select alert

--- script/script.js
+++ script/script.js
@@ -2,10 +2,11 @@
 var generateBtn = document.querySelector("#generate");
     console.log(generateBtn);
 
+// List of all variables
 var upCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var specialChar = [" ", "`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "", "|", ";", ":", "'", ",", "<", ".", ">", "/", "?"];
+var specialChar = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "", "|", ";", ":", "'", ",", "<", ".", ">", "/", "?"];
 
 // Write password to the #password input
 function writePassword() {
@@ -14,18 +15,28 @@ function writePassword() {
   passwordText.value = password;
 }
 
+// Function to generate random PW
 function generatePassword() {
   var pwLength = prompt("Please make password between 8 to 128 characters long.");
     console.log(pwLength);
-  var upCaseAns = confirm("Do you want use Upper Case letters?");
-    console.log(upCaseAns);
-  var lowCaseAns = confirm("Do you want use Lower Case Letters?");
-    console.log(lowCaseAns);
-  var numAns = confirm("Do you want to use Numbers?");
-    console.log(numAns);
-  var specialCharAns = confirm("Do you want to use Special Characters?");
-    console.log(specialCharAns);
 
+      // Confirms to ask the user what to include in the PW
+      var upCaseAns = confirm("Click OK to use \"UPPER\" case letters OR Click Cancel");
+        console.log(upCaseAns);
+      var lowCaseAns = confirm("Click OK to use \"lower\" case letters OR Click Cancel");
+        console.log(lowCaseAns);
+      var numAns = confirm("Click OK to use \"Numbers\ OR Click Cancel");
+        console.log(numAns);
+      var specialCharAns = confirm("Click OK to use \"Special\" Characters OR Click Cancel");
+        console.log(specialCharAns);
+
+      // Alert to notify use that ONE selection must be made
+      if (!upCaseAns && !lowCaseAns && !numAns && !specialCharAns) {
+        alert("You must choose at least one option!");
+        console.log("You must choose at least one option!");
+      }
+      
 }
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
