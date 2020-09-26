ca3abd1a0a65e766591681028c6fe8193f4503a5
Michelle McConville <mchelmconvl@gmail.com>
2020-09-26 18:45:38 -0500

    array change

--- script/script.js
+++ script/script.js
@@ -3,10 +3,10 @@ var generateBtn = document.querySelector("#generate");
     console.log(generateBtn);
 
 // List of all variables
-var upCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var lowCase = "abcdefghijklmnopqrstuvwxyz".split("");
 var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var specialChar = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "", "|", ";", ":", "'", ",", "<", ".", ">", "/", "?"];
+var specialChar = "`~!@#$%^&*()-_=+[{]}\"|;:',<.>/?".split("");
 var maxArray = [];
 var newUserPw = "";
 
